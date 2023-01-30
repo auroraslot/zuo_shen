e8cffbfd1bcf937189d2538c71af15e55beca283
tangweize <tangweize@pailifan.com>
2023-01-30 15:56:26 +0800

    add 链表实现栈

--- src/main/java/pers/tz/repetition01/class03/Class04_ListImplementStack.java
+++ src/main/java/pers/tz/repetition01/class03/Class04_ListImplementStack.java
@@ -1,5 +1,7 @@
 package pers.tz.repetition01.class03;
 
+import pers.tz.common.Node;
+
 /**
  * <h3>title：链表实现栈</h3>
  * <h3 style='margin-top: 10px;'>要求：</h3>
@@ -14,6 +16,40 @@ package pers.tz.repetition01.class03;
  */
 public class Class04_ListImplementStack {
 
+    public static class MyStack {
+        private Node head;
+        private int size;
 
+        public void push(int value) {
+            Node node = new Node(value);
+            if (head == null) {
+                head = node;
+            } else {
+                node.next = head;
+                head = node;
+            }
+            size++;
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int num = head.value;
+            head = head.next;
+            size--;
+            return num;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+    }
 
 }
